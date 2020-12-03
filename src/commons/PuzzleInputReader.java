4bfbf9cf1902b615ff630047a70874f120b9141b
Peter Pepo <peterpepo1@gmail.com>
2020-12-03 14:26:05 +0100

    PuzzleInputReader possible leaks fix
    * Fixes possible leaks in FileReader and BufferedReader calls when dealing with puzzle input files

--- src/commons/PuzzleInputReader.java
+++ src/commons/PuzzleInputReader.java
@@ -5,30 +5,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PuzzleInputReader {
-	
+
 	private List<String> puzzle_input_lines = new ArrayList<>();
-	
-	public PuzzleInputReader(String filename) {	
+
+	public PuzzleInputReader(String filename) {
 		File file_to_read_from = new File(filename);
-		
+
+		FileReader puzzleInputFileReader = null;
+		BufferedReader puzzleInputBufferedReader = null;
+
 		try {
-			BufferedReader puzzle_input_buffered_reader = new BufferedReader(new FileReader(file_to_read_from));
-			
+			puzzleInputFileReader = new FileReader(file_to_read_from);
+			puzzleInputBufferedReader = new BufferedReader(puzzleInputFileReader);
+
 			String current_line;
-			
-			while ((current_line = puzzle_input_buffered_reader.readLine()) != null) {
+
+			while ((current_line = puzzleInputBufferedReader.readLine()) != null) {
 				puzzle_input_lines.add(current_line);
 			}
-				
-				
+
 		} catch (FileNotFoundException e) {
 			System.err.println(String.format("Specified file %s not found.", filename));
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println(String.format("Error reading from file %s.", filename));
+		} finally {
+			try {
+				puzzleInputBufferedReader.close();
+			} catch (Exception e) {
+				System.err.println(String.format("Error closing BufferedReader for %s.", filename));
+			}
+			try {
+				puzzleInputFileReader.close();
+			} catch (Exception e) {
+				System.err.println(String.format("Error closing FileReader for %s.", filename));
+			}
 		}
 	}
-	
+
 	public List<String> getListOfLines() {
 		return puzzle_input_lines;
 	}
